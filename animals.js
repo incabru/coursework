a4276f1b16101e4d3b658aff5aff415caa225777
incabru <40479334@live.napier.ac.uk>
2022-04-28 09:57:53 +0100

    Update
    Coursework

---
+++ animals.js
@@ -0,0 +1,168 @@
+// 1st Flashcard
+function answer() {
+    document.getElementById('answer').style.visibility = 'visible';
+    
+}
+
+function hide() {
+    document.getElementById('answer').style.visibility = 'hidden';
+}
+
+function next() {
+    document.getElementById('box_2').style.display = 'block';
+    document.getElementById('box_1').style.display = 'none';
+}
+
+// 2nd Flashcard
+function answer2() {
+    document.getElementById('answer2').style.visibility = 'visible';
+}
+
+function hide2() {
+    document.getElementById('answer2').style.visibility = 'hidden';
+}
+
+function next2() {
+    document.getElementById('box_3').style.display = 'block';
+    document.getElementById('box_2').style.display = 'none';
+}
+
+// 3rd Flashcard
+function answer3() {
+    document.getElementById('answer3').style.visibility = 'visible';
+}
+
+function hide3() {
+    document.getElementById('answer3').style.visibility = 'hidden';
+}
+
+function next3() {
+    document.getElementById('box_4').style.display = 'block';
+    document.getElementById('box_3').style.display = 'none';
+}
+
+// 4th Flashcard
+function answer4() {
+    document.getElementById('answer4').style.visibility = 'visible';
+}
+
+function hide4() {
+    document.getElementById('answer4').style.visibility = 'hidden';
+}
+
+function next4() {
+    document.getElementById('box_5').style.display = 'block';
+    document.getElementById('box_4').style.display = 'none';
+}
+
+// 5th Flashcard
+function answer5() {
+    document.getElementById('answer5').style.visibility = 'visible';
+}
+
+function hide5() {
+    document.getElementById('answer5').style.visibility = 'hidden';
+}
+
+function next5() {
+    document.getElementById('box_6').style.display = 'block';
+    document.getElementById('box_5').style.display = 'none';
+}
+
+// 6th Flashcard 
+function answer6() {
+    document.getElementById('answer6').style.visibility = 'visible';
+}
+
+function hide6() {
+    document.getElementById('answer6').style.visibility = 'hidden';
+}
+
+function next6() {
+    document.getElementById('box_7').style.display = 'block';
+    document.getElementById('box_6').style.display = 'none';
+}
+
+// 7th Flashcard
+function answer7() {
+    document.getElementById('answer7').style.visibility = 'visible';
+}
+
+function hide7() {
+    document.getElementById('answer7').style.visibility = 'hidden';
+}
+
+function next7() {
+    document.getElementById('box_8').style.display = 'block';
+    document.getElementById('box_7').style.display = 'none';
+}
+
+// 8th Flashcard
+function answer8() {
+    document.getElementById('answer8').style.visibility = 'visible';
+}
+
+function hide8() {
+    document.getElementById('answer8').style.visibility = 'hidden';
+}
+
+function next8() {
+    document.getElementById('box_9').style.display = 'block';
+    document.getElementById('box_8').style.display = 'none';
+}
+
+// 9th Flashcard
+function answer9() {
+    document.getElementById('answer9').style.visibility = 'visible';
+}
+
+function hide9() {
+    document.getElementById('answer9').style.visibility = 'hidden';
+}
+
+function next9() {
+    document.getElementById('box_10').style.display = 'block';
+    document.getElementById('box_9').style.display = 'none';
+}
+
+// 10th Flashcard
+function answer10() {
+    document.getElementById('answer10').style.visibility = 'visible';
+}
+
+function hide10() {
+    document.getElementById('answer10').style.visibility = 'hidden';
+}
+
+function next10() {
+    document.getElementById('box_11').style.display = 'block';
+    document.getElementById('box_10').style.display = 'none';
+}
+
+// 11th Flashcard
+function answer11() {
+    document.getElementById('answer11').style.visibility = 'visible';
+}
+
+function hide11() {
+    document.getElementById('answer11').style.visibility = 'hidden';
+}
+
+function next11() {
+    document.getElementById('box_12').style.display = 'block';
+    document.getElementById('box_11').style.display = 'none';
+}
+
+// 12th Flashcard
+function answer12() {
+    document.getElementById('answer12').style.visibility = 'visible';
+}
+
+function hide12() {
+    document.getElementById('answer12').style.visibility = 'hidden';
+}
+
+function next12() {
+    document.getElementById('box_13').style.display = 'block';
+    document.getElementById('box_12').style.display = 'none';
+}
