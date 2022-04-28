e1c33df5b4229e1ad22350274b28ff4d13a142fb
incabru <40479334@live.napier.ac.uk>
2022-04-28 13:21:02 +0100

    Update

--- animals.js
+++ animals.js
@@ -1,168 +1,207 @@
+// @author: Sergio Rodriguez Chavez  
+
+
 // 1st Flashcard
 function answer() {
     document.getElementById('answer').style.visibility = 'visible';
+    document.getElementById('sound2').play();
     
 }
 
 function hide() {
     document.getElementById('answer').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next() {
     document.getElementById('box_2').style.display = 'block';
     document.getElementById('box_1').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 2nd Flashcard
 function answer2() {
     document.getElementById('answer2').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide2() {
     document.getElementById('answer2').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next2() {
     document.getElementById('box_3').style.display = 'block';
     document.getElementById('box_2').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 3rd Flashcard
 function answer3() {
     document.getElementById('answer3').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide3() {
     document.getElementById('answer3').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next3() {
     document.getElementById('box_4').style.display = 'block';
     document.getElementById('box_3').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 4th Flashcard
 function answer4() {
     document.getElementById('answer4').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide4() {
     document.getElementById('answer4').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next4() {
     document.getElementById('box_5').style.display = 'block';
     document.getElementById('box_4').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 5th Flashcard
 function answer5() {
     document.getElementById('answer5').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide5() {
     document.getElementById('answer5').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next5() {
     document.getElementById('box_6').style.display = 'block';
     document.getElementById('box_5').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 6th Flashcard 
 function answer6() {
     document.getElementById('answer6').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide6() {
     document.getElementById('answer6').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next6() {
     document.getElementById('box_7').style.display = 'block';
     document.getElementById('box_6').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 7th Flashcard
 function answer7() {
     document.getElementById('answer7').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide7() {
     document.getElementById('answer7').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next7() {
     document.getElementById('box_8').style.display = 'block';
     document.getElementById('box_7').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 8th Flashcard
 function answer8() {
     document.getElementById('answer8').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide8() {
     document.getElementById('answer8').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next8() {
     document.getElementById('box_9').style.display = 'block';
     document.getElementById('box_8').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 9th Flashcard
 function answer9() {
     document.getElementById('answer9').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide9() {
     document.getElementById('answer9').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next9() {
     document.getElementById('box_10').style.display = 'block';
     document.getElementById('box_9').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 10th Flashcard
 function answer10() {
     document.getElementById('answer10').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide10() {
     document.getElementById('answer10').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next10() {
     document.getElementById('box_11').style.display = 'block';
     document.getElementById('box_10').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 11th Flashcard
 function answer11() {
     document.getElementById('answer11').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide11() {
     document.getElementById('answer11').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next11() {
     document.getElementById('box_12').style.display = 'block';
     document.getElementById('box_11').style.display = 'none';
+    document.getElementById('sound1').play();
 }
 
 // 12th Flashcard
 function answer12() {
     document.getElementById('answer12').style.visibility = 'visible';
+    document.getElementById('sound2').play();
 }
 
 function hide12() {
     document.getElementById('answer12').style.visibility = 'hidden';
+    document.getElementById('sound3').play();
 }
 
 function next12() {
     document.getElementById('box_13').style.display = 'block';
     document.getElementById('box_12').style.display = 'none';
+    document.getElementById('sound1').play();
 }
